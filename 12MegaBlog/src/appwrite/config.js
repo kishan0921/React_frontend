cb0330be41bea02bd8bc547460d7ea1ce22d2ccb
kishan Sharma <71210835+kishan0921@users.noreply.github.com>
2025-08-15 16:23:07 +0000

    hjjj

--- 12MegaBlog/src/appwrite/config.js
+++ 12MegaBlog/src/appwrite/config.js
@@ -124,15 +124,90 @@ export class Service{
     }
 
 
-    // Now , ab mujhe sirf wo post chahiyhe jiska status active h
+    // Now , ab mujhe sirf wo post chahiyhe jiska status "active" h
     // to query wagera use krna hoga
+    // ab wohi async use krenge
+    // yaha "queries" variable name hai. 
+    // inside queries hum value passwd kar rahe h, ki queries hai 
+    // and mujhe equal waala sirf chahiye, and status - equal honi chahiye.
+    async getPosts (queries = [Query.equal('status', "equal")]){
+        // https://appwrite.io/docs/products/databases/queries
+        try {
+            // jaise uppar se krte aa rahe h, waise hi krna h 
+            // bss yaha ab hum listDocuments () method use krenge
+            return await this.databases.listDocuments(
+                // ab yaha hum 
+            //[DATABASE_ID], [COLLECTION_ID],[Queries] denge
+            conf.appwriteDatabaseId,
+            conf.appwriteCollectionId,
+            // aur yaha queries fill kr diye, and yaha jo uppar value assigned h wo aayega.
+            queries,
+            )
+            
+        } catch (error) {
+            console.log("Appwrite serive :: getPosts :: error", error);
+            return false
+        }
+    }
+
+
+
+    // Part - 02 : File upload service 
+    // File upload waala ek method bana lete hai.
+    async uploadFile (file){
+        try {
+            // ab file create krna hai, to .createFile() method ka use kr lenge.
+            // kyuki async hai , to await use krna hoga.
+            return await this.bucket.createFile(
+                // Ab bucket id 
+                conf.appwriteBucketId,
+                // Ab unique Id de do, ye uppar import kr rakhe hai
+                ID.unique(),
+                // ab 3rd parameter jo hoga wo File hota h
+                file
+                // bas itna hi kaam h
+            )
+            
+        } catch (error) {
+            console.log("Appwrite serive :: uploadFile :: error", error);
+            return false
+        }
+    }
+
+
+    // Ab file delete waala method bana lete hai
+    // delete krte time fileId dena hoga.
+    async deleteFile(fileId){
+
+        try {
+            await this.bucket.deleteFile(
+                conf.appwriteBucketId,
+                fileId
+            )
+            return true
+        } catch (error) {
+            console.log("Appwrite serive :: deleteFile :: error", error);
+            return false
+        }
+    }
+
+
+    // Ab ek aur service hum bana lete hai, file Preview ka
+    getFilePreview(fileId){
+        return this.bucket.getFilePreview(
+            conf.appwriteBucketId,
+            fileId
+        )
+    }
+
+    
+
+
+
 
 }
 
 // Ab kyuki "new" keyboard ka use krke service wagera bannai h, to constructor bana hi padega
-
-
-
 // Ab ek object bana lete hai...using new keyword
 const service = new Service()
 
