f76d83dbe7106713f4a6e27e5f6ed4614ecdaf77
kishan Sharma <71210835+kishan0921@users.noreply.github.com>
2025-08-16 06:16:52 +0000

    ss

--- 12MegaBlog/src/main.jsx
+++ 12MegaBlog/src/main.jsx
@@ -3,8 +3,17 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+// step 1: chalo provider le akr aate h
+import { Provider } from 'react-redux'
+// step 2: store bhi laagega
+import store from "./store/store.js"
+// step 3- App ko wrap kr do provider ke ander 
+
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Provider>
+     <App />
+    </Provider>
   </StrictMode>,
 )
