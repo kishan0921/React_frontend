8dc952975a6cd90596269e078ded10eff758e338
kishan Sharma <71210835+kishan0921@users.noreply.github.com>
2025-08-16 11:55:29 +0000

    ffff

--- 12MegaBlog/src/components/Login.jsx
+++ 12MegaBlog/src/components/Login.jsx
@@ -1,3 +1,9 @@
+// Note: Login and sigup .jsx dono react form hook 
+// pe based hai and same code likha gaya hai dono me ...
+// // 1 samjh lo 2nd file easily smjh jaaoge
+
+
+
 // state to change krna hoga , so useState le lete hai
 import React, {useState} from 'react'
 // login ke baad kahi to bhejoge user ko , 
@@ -42,11 +48,18 @@ function Login() {
             // then authService se login method ko call krenge and data ko send krenge
             // and jo response aayega wo aayega ek session.
             const session = await authService.login(data)
-            // agar session aaya hai then
+            // agar session aaya hai then, then user login hai and nahi aaya hai mtlb
+            // user login nahi hai.
             if (session) {
-                
+                // agar session hai, to hum user data nikalenge
+                // authService ke ander getCurrentUser() method hai.
                 const userData = await authService.getCurrentUser()
+                // agar ab, humare pass userData aaya hai then 
+                // dispatch krna hoga, and authlogin ke ander UserData pass kr diya.
                 if(userData) dispatch(authLogin(userData));
+                // yaha tak, iss step tk agar user aa gaya h, then 
+                // ab mtlb user login hai, then yaha kyu rakhna hai isse,
+                // login ho gaya h user, then (/) yaha pe naviagte kr do user ko
                 navigate("/")
             }
         } catch (error) {
@@ -76,16 +89,37 @@ function Login() {
                         Sign Up
                     </Link>
         </p>
+        // agar error aayega tabhi && ke baad jo likha hai , show krenge nahi to nahi.
         {error && <p className="text-red-600 mt-8 text-center">{error}</p>}
+        
+        
+        {/* // Ab interesting Story start hoti hai yaha se, form ki  */}
+        {/* // form ke ander 1 major activity hoti hai, onSubmit
+        // and form jab bhi submit hoga then , always handleSubmit() use hoga, and ye ek method hai 
+        // and isske ander hum apna method send krte hai, ki iss tarah se hum submit handle krengege. 
+        // not yaha handleSubmit ek event hai. */}
+
+        //Note: why we use handleSubmit ?
+        - form me jitne bhi input field wagera hum denge,to yaha agar hum register() ka use krte hai,
+         krte hai to , automatically form me jitne bhi input field wagera hum denge, usska state manage krne ki zarurate nahi h
+         //automatically, from ke saare value register pick krega and without , managing state 
+         // apne aap handleSubmit call krte time, saare value wo le lega. 
         <form onSubmit={handleSubmit(login)} className='mt-8'>
             <div className='space-y-5'>
+                {/* // chalo ek input field banayenge, and self closing hai ye <Input/> */}
                 <Input
                 label="Email: "
                 placeholder="Enter your email"
                 type="email"
+                // {...register} - aise hi use krte hai , always 
+                // // and register ke ander (1st- key value, 2nd - object)
                 {...register("email", {
-                    required: true,
-                    validate: {
+                // 2nd value hai, object
+                // and obejct me hum bahut saare options pass krte hai.
+                //1st option true h    
+                required: true,
+                // 2nd option validate h
+                validate: {
                         matchPatern: (value) => /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(value) ||
                         "Email address must be a valid address",
                     }
